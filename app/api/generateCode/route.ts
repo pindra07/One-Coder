0e91c2b1858034f68d60839bc4e30096a98dd23f
Priyanshu Indra <indrapriyanshu@gmail.com>
2025-12-29 14:40:46 +0530

    open ai sdk added

--- app/api/generateCode/route.ts
+++ app/api/generateCode/route.ts
@@ -1,10 +1,20 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-export async function POST (request: Request) {
-    const {prompt} = await request.json()
-    console.log(prompt)
-    return NextResponse.json({message: "Hello World"})
+export async function POST(request: Request) {
+  const { prompt } = await request.json();
+  console.log(prompt);
+  const response = await openai.chat.completions.create({
+    model: "gpt-4",
+    messages: [
+      { role: "system", content: "You are a react developer" },
+      { role: "user", content: prompt },
+    ],
+    stream: true
+  });
+  return NextResponse.json({ message: "Hello World" });
 }
-
-
