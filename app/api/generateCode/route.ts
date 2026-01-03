36d218f1426a73fe7a2f571db3dd8ac11500728d
Priyanshu Indra <indrapriyanshu@gmail.com>
2026-01-03 17:26:29 +0530

    new

--- app/api/generateCode/route.ts
+++ app/api/generateCode/route.ts
@@ -10,31 +10,32 @@ export async function POST(request: Request) {
     model: "gemini-3-flash-preview",
     contents: "You're a react developer",
     config: {
-      systemInstruction: "You are a react developer write only code for react",
+      systemInstruction:
+        "You are a react developer write only code for react. You will be asked to build a react component. - ONLY return react code for the app. DO NOT return ```jsx or any other texty. ONLY return the code. & USE ONLY tailwind for styling no component library",
     },
   });
 
   const stream = new ReadableStream({
     async start(controller) {
-        const encoder = new TextEncoder();
-        try {
-            for await(const chunk of response) {
-                const text = chunk.text;
-                if(text) {
-                    controller.enqueue(encoder.encode(text));
-                }
-            }
-        } catch(e) {
-            controller.error(e);
-        } finally {
-            controller.close();
+      const encoder = new TextEncoder();
+      try {
+        for await (const chunk of response) {
+          const text = chunk.text;
+          if (text) {
+            controller.enqueue(encoder.encode(text))
+          }
         }
+      } catch (e) {
+        controller.error(e);
+      } finally {
+        controller.close();
+      }
     },
-  })
+  });
 
-  console.log(stream)
-  
-  return NextResponse.json(stream, {
-    headers: {"Content-Type": "text/plain; charset=utf8"},
+  console.log(stream);
+
+  return new NextResponse(stream, {
+    headers: { "Content-Type": "text/plain; charset=utf8" },
   });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,57 @@
-"use client"
+"use client";
 import { useState } from "react";
 import { Sandpack } from "@codesandbox/sandpack-react";
-import { freeCodeCampDark } from "@codesandbox/sandpack-themes";
-
 
 export default function Home() {
-  const [prompt, setPrompt] = useState("")
-  const [generatedCode, setGeneratedCode] = useState("")
+  const [prompt, setPrompt] = useState("");
+  const [generatedCode, setGeneratedCode] = useState("");
 
   async function generateCode(e: React.FormEvent<HTMLFormElement>) {
-    setGeneratedCode("")
-    e.preventDefault()
+    setGeneratedCode("");
+    e.preventDefault();
     const res = await fetch("/api/generateCode", {
       method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({prompt})
-    })
-    console.log(res.body)
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ prompt }),
+    });
+    console.log(res.body);
     const reader = res.body?.getReader();
-    const decoder = new TextDecoder()
+    const decoder = new TextDecoder();
 
-    while(true) {
+    while (true) {
       //@ts-ignore
-      const {value, done} = await reader?.read();
-      if(done) {
+      const { value, done } = await reader?.read();
+      if (done) {
         break;
       }
 
-      const chunk = decoder.decode(value, {stream: true});
-      setGeneratedCode((prev) => prev + chunk)
+      const chunk = decoder.decode(value, { stream: true });
+      setGeneratedCode((prev) => prev + chunk);
     }
+    console.log(generatedCode)
   }
   return (
     <div className="mx-auto max-w-5xl mt-20 space-y-4">
       <h1 className="font-bold text-3xl">One Coder</h1>
       <pre>Generate landing page with one click...</pre>
       <form onSubmit={generateCode}>
-      <input
-        value={prompt}
-        type="text"
-        onChange={(e)=> setPrompt(e.target.value)}
-        placeholder="put in your prompt"
-        className="border p-2 rounded-md"
-      />
-      <button className="text-white bg-black m-3 p-2 rounded-lg cursor-pointer ">Submit</button>
+        <input
+          value={prompt}
+          type="text"
+          onChange={(e) => setPrompt(e.target.value)}
+          placeholder="put in your prompt"
+          className="border p-2 rounded-md"
+        />
+        <button className="text-white bg-black m-3 p-2 rounded-lg cursor-pointer ">
+          Submit
+        </button>
       </form>
-      <Sandpack 
-      theme={freeCodeCampDark}
-      template="react-ts"
-      files={{
-        "/App.tsx": generatedCode
-      }} 
+      <Sandpack
+        template="react-ts"
+        files={{
+          "/App.tsx": generatedCode,
+          
+        }}
       />
     </div>
   );
